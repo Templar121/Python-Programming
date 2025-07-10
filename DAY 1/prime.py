d8031769f64ddbeaeb42a567b44df13c7f2b6a52
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-07-10 20:52:20 +0530

    prime added

---
+++ DAY 1/prime.py
@@ -0,0 +1,18 @@
+def is_prime(n):
+    if n == 1:
+        print("Neither Prime nor Composite")
+        return
+    if n == 2:
+        print("Is Prime")
+        return
+    if n % 2 == 0:
+        print("Is Not Prime or Is Composite")
+        return
+        
+    for i in range(3, int(n ** 0.5) + 1, 2):
+        if n % i == 0:
+            print("Is Not Prime or Is Composite")
+        return 
+    print("Is Prime")
+    
+is_prime(25)
